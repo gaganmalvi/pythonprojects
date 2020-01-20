7783d95baaf06727a882f1b7b0cb64f6c10977d9
Gagan Malvi <let1tra1nmacra1n@gmail.com>
2020-01-20 22:26:25 +0530

    Update set-1-01.py

--- set-1-01.py
+++ set-1-01.py
@@ -1,6 +1,6 @@
 import pandas as pd
 d1 = {'name':['Tahira','Gurjyot','Anusha','Jacob'],
-      'marks':[70,60,40,50]
+      'marks':[70,60,40,50],
       'class':[28,36,41,32]}
 dfd = pd.DataFrame(d1)
 d2 = dfd.pivot(index = 'name',columns = 'marks',values = 'class')
